11e95991d6617260eab83e1b9e1d6b8b460618c5
Pavel Cherkashin <pcherkashin@gmail.com>
2022-08-31 22:02:54 +0300

    add h1

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,7 @@ export const App = () => (
     <Header />
     <div className="min-h-full">
       <div className="max-w-4xl mx-auto p-2">
+        <h1>Meteor Wallet</h1>
         <ContactForm />
         <ContactList />
       </div>
